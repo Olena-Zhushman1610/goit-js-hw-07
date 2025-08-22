120c57815412a27d0e89d95043b8a63856470eb7
Olena <alenguhm@gmail.com>
2025-08-22 13:32:20 +0300

    add task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const body = document.body;
+button.addEventListener("click", (event) => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+});
